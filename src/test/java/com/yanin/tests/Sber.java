23d4176cd382a0a84dc541a4cf27aacfe9f4e908
TropicoExodus <a.yanin1501@gmail.com>
2023-11-30 19:58:39 +0300

    Создан проект maven

--- src/test/java/com/yanin/tests/Sber.java
+++ src/test/java/com/yanin/tests/Sber.java
@@ -1,4 +1,15 @@
 package com.yanin.tests;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Sber {
+
+    @Test
+    public void test(){
+        // WebDriver path
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+    }
 }
