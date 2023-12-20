ef8ae818db6c3bb426875f2051b1a657f1e42e06
TropicoExodus <a.yanin1501@gmail.com>
2023-12-20 19:07:47 +0300

    Создан тест для итогового задания

--- src/test/java/com/yanin/tests/RegardAutomationTestTwo.java
+++ src/test/java/com/yanin/tests/RegardAutomationTestTwo.java
@@ -1,6 +1,5 @@
 package com.yanin.tests;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,12 +8,11 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.List;
+
 import java.util.concurrent.TimeUnit;
 
-public class RegardAutomationTest {
+public class RegardAutomationTestTwo {
 
     WebDriver driver;
     WebDriverWait waitTime;
@@ -25,80 +23,69 @@ public class RegardAutomationTest {
         driver = new ChromeDriver();
 
 
-        driver.get("http://regard.ru");
+//        driver.navigate().to("https://www.sberbank.ru/ru/person");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         waitTime = new WebDriverWait(driver, 10, 2000);
-
+        driver.get("http://regard.ru");
 
     }
 
     @Test
     public void test() {
 
-
-
-        // Открываем бургер-меню "Каталог"
-        String baseCatalogXpath = "//div[@class='NavigationBar_burger__j7lZE']";
-        WebElement baseCatalog = driver.findElement(By.xpath(baseCatalogXpath));
-        baseCatalog.click();
-
-        // Выбираем раздел каталога
-        String subCatalogXpath = "//div//a[@data-id='1536']";
-        WebElement subCatalog = driver.findElement(By.xpath(subCatalogXpath));
-        subCatalog.click();
-
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        // Открываем бургер-меню "Каталог"
+        WebElement baseCatalog = driver.findElement(By.xpath("//div[@class='NavigationBar_burger__j7lZE']"));
+        baseCatalog.click();
 
+        // Выбираем раздел каталога
+        WebElement subCatalog = driver.findElement(By.xpath("//div//a[@data-id='1026']"));
+        subCatalog.click();
 
-        //waitTime.until(ExpectedConditions.attributeContains())
-        // Проверяем загрузился ли раздел и выбираем категорию товаров
-        WebElement subCatalogTitle = driver.findElement(By.xpath("//div[@class='div page_title h1']"));
-        String titleClassValue = subCatalogTitle.getAttribute("class");
-        String exceptedClassValue = "div page_title h1";
-        Assert.assertEquals("Раздел не загрузился", exceptedClassValue, titleClassValue);
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
-        String categoryCatalogXpath = "//p[@class='CardCategory_title__K2CCX' and contains(text(),'Видеокарты')]";
-        WebElement categoryCatalog = driver.findElement(By.xpath(categoryCatalogXpath));
+        // Выбираем категорию товаров
+        WebElement categoryCatalog = driver.findElement(By.xpath("//p[@class='CardCategory_title__K2CCX' and contains(text(),'Клавиатуры')]"));
         categoryCatalog.click();
 
-
-
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         // Задаем фильтр по цене
-        String priceFilterXpath = "//div/input[@class='RangeSelector_input__26nqz range-selector-input' and @name='min'][1]";
-        WebElement priceFilter = driver.findElement(By.xpath(priceFilterXpath));
-        priceFilter.sendKeys("20000");
+        WebElement priceFilter = driver.findElement(By.xpath("//div/input[@class='RangeSelector_input__26nqz range-selector-input' and @name='min'][1]"));
+        priceFilter.sendKeys("2000");
 
         // Выбираем производителя Gigabyte
-        String setVendorXpath = "//label[contains(text(),'Gigabyte')]";
-        WebElement setVendor = driver.findElement(By.xpath(setVendorXpath));
+        WebElement setVendor = driver.findElement(By.xpath("//label[contains(text(),'A4Tech')]"));
         setVendor.click();
 
 
         // Проверяем количество товаров на странице
         String productsOnPage = String.valueOf(driver.findElement(By.xpath("//div/span[@class='PaginationViewChanger_countSetter__count__65Dji' and contains(text(),'24')]")));
         Assert.assertTrue("Количество товаров не соответствует 24", productsOnPage.contains("24"));
-
-
-
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         // Сохраняем наименование первого товара
-        String findFirstProductNameXpath = "//div[text()[contains(.,'Видеокарта')]][1]";
-        WebElement findFirstProductName = driver.findElement(By.xpath(findFirstProductNameXpath));
+        WebElement findFirstProductName = driver.findElement(By.xpath("//div[text()[contains(.,'Клавиатура')]][1]"));
         String firstProductName = findFirstProductName.getText();
 
         System.out.println("Наименование первого товара: " + firstProductName);
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
         // Ищем по сохраненному наименованию
         WebElement searchBox = driver.findElement(By.id("searchInput")); // Укажите правильный id поисковой строки
@@ -106,26 +93,23 @@ public class RegardAutomationTest {
         searchBox.click();
         searchBox.sendKeys(Keys.ENTER);
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         // Ожидаем результат поиска и проверяем количество товаров
         WebElement oneInResult = driver.findElement(By.xpath("//div[text()='Выбран 1 товар']"));
         Assert.assertTrue("Выбрано больше 1 товара", oneInResult.isDisplayed());
 
         // Проверяем соответствие наименования найденного товара ранее сохраненному
-        String searchProductNameXpath = "//div[@title and text()[contains(.,'Видеокарта')]]";
-        WebElement searchProductName = driver.findElement(By.xpath(searchProductNameXpath));
+        WebElement searchProductName = driver.findElement(By.xpath("//div[@title and text()[contains(.,'Клавиатура')]]"));
         String realProductName = searchProductName.getText();
 
         Assert.assertEquals("Наименование найденного товара не соответствует ранее сохраненному",firstProductName, realProductName);
+//        Assert.assertEquals(firstProductName,findFirstProductName.getText());
 
 
     }
-
-
 }
